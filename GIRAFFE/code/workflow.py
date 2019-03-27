d385370ee34d9e72ece5a7467bd2bbc005b8d3eb
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-03-27 18:30:44 +0100

    GiraffeTools init

---
+++ GIRAFFE/code/workflow.py
@@ -0,0 +1,19 @@
+#This is a Nipype generator. Warning, here be dragons.
+#!/usr/bin/env python
+
+import sys
+import nipype
+import nipype.pipeline as pe
+
+
+
+
+#Create a workflow to connect all those nodes
+analysisflow = nipype.Workflow('MyWorkflow')
+
+
+#Run the workflow
+plugin = 'MultiProc' #adjust your desired plugin here
+plugin_args = {'n_procs': 1} #adjust to your number of cores
+analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
+analysisflow.run(plugin=plugin, plugin_args=plugin_args)
