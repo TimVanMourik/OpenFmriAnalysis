b14360b1452f56fdd982bff5a3571505b1475dee
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-03-27 18:36:22 +0100

    Design matrix

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -8,9 +8,24 @@ import nipype.pipeline as pe
 
 
 
+
+
+
+
+
+
+
+
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-
+analysisflow.connect(tvm_design_empty, "o_DesignMatrix", tvm_design_stimulus, "i_DesignMatrix")
+analysisflow.connect(tvm_design_stimulus, "o_DesignMatrix", tvm_design_constant, "i_DesignMatrix")
+analysisflow.connect(tvm_design_constant, "o_DesignMatrix", tvm_design_highpassFilter, "i_DesignMatrix")
+analysisflow.connect(tvm_design_highpassFilter, "o_DesignMatrix", tvm_design_motionRegression, "i_DesignMatrix")
+analysisflow.connect(tvm_design_motionRegression, "o_DesignMatrix", tvm_design_retroicor, "i_DesignMatrix")
+analysisflow.connect(tvm_design_retroicor, "o_DesignMatrix", tvm_design_retroicor_1, "i_DesignMatrix")
+analysisflow.connect(tvm_design_retroicor_1, "o_DesignMatrix", tvm_regressConfounds, "i_DesignMatrix")
+analysisflow.connect(tvm_realignFunctionals, "o_OutputDirectory", tvm_regressConfounds, "i_FunctionalFolder")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
